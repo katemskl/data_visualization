f30a4bb7413d87f632e656c5a242cab8dd6f31cd
katemskl <moskal4uk.k@gmail.com>
2022-05-25 20:23:56 +0300

    improved

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 
-plt.scatter(2, 4, s=200)
+x_values = list(range(1, 1000))
+y_values = [x**2 for x in x_values]
+plt.scatter(x_values, y_values, edgecolors='none', s=40, c=y_values, cmap=plt.cm.Blues)
 
 # Назначение заголовка диаграммы и меток осей
 plt.title('Square Numbers', fontsize=24)
@@ -10,4 +12,8 @@ plt.ylabel('Square of Value', fontsize=14)
 # Назначение размера шрифта делений на осях
 plt.tick_params(axis='both', which='major', labelsize=14)
 
+# Назначение диапазона каждой оси
+plt.axis([0, 1100, 0, 1100000])
+
+# plt.savefig('squares_plot.png', bbox_inches='tight')
 plt.show()
