a4b0baabcce0787e30c229aea023f8ab9e062346
katemskl <moskal4uk.k@gmail.com>
2022-05-27 21:38:16 +0300

    something wrong

--- rw_visual.py
+++ rw_visual.py
@@ -4,7 +4,26 @@ from random_walk import RandomWalk
 
 # Построение случайного блуждания и нанесение точек на диаграмму
 
-rw = RandomWalk(100)
-rw.fill_walk()
-plt.scatter(rw.x_values, rw.y_values, s=10)
-plt.show()
+keep_running = 'y'
+
+while True:
+    rw = RandomWalk(100)
+    rw.fill_walk()
+    points_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=points_numbers, cmap=plt.cm.Blues, edgecolors='none', s=10)
+
+    # Выделение первой и последней точек
+    plt.scatter(0, 0, c='green', edgecolors='none', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+    # Удаление осей
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
+
+    plt.show()
+
+    keep_running = input('Make another walk? (y/n): ')
+    if keep_running.lower() == 'n':
+        break
+
+
