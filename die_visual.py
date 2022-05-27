f29e64cb33324966730ce7eda920d98a688cdb7e
katemskl <moskal4uk.k@gmail.com>
2022-05-27 22:25:35 +0300

    added code

---
+++ die_visual.py
@@ -0,0 +1,33 @@
+from die import Die
+import pygal
+
+
+# Создание кубика D6
+
+die = Die()
+
+# Моделирование серии бросков с сохранением результатов в списке
+results = []
+for roll_num in range(1000):
+    results.append(die.roll())
+
+# Анализ результатов
+# frequencies = dict()
+# for value in range(1, die.num_sides + 1):
+#     frequencies[value] = results.count(value)
+
+frequencies = []
+for value in range(1, die.num_sides + 1):
+    frequencies.append(results.count(value))
+
+# Визуализация результатов
+
+hist = pygal.Bar()
+
+hist.title = 'Results of rolling one D6 1000 times'
+hist.x_labels = ['1', '2', '3', '4', '5', '6']
+hist.x_title = 'Result'
+hist.y_title = 'Frequency of Result'
+
+hist.add('D6', frequencies)
+hist.render_to_file('die_visual.svg')
